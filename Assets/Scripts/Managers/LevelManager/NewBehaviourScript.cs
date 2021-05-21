192271f2e6746a2120903d805b6c380683ac1551
Myles <mylesblasonato988@hotmail.com>
2021-05-21 15:39:55 +1000

    Mobile Framework

--- Assets/Scripts/Managers/LevelManager/NewBehaviourScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName="Create Level Bank")]
-public class LevelBank : ScriptableObject
-{
-
-}
