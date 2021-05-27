fd4086b14b5879e09c3655f3111a23e0f19ed72d
Myles <mylesblasonato988@hotmail.com>
2021-05-27 17:10:03 +1000

    updated with lerp

--- Assets/Scripts/Controllers/FlickController.cs
+++ Assets/Scripts/Controllers/FlickController.cs
@@ -6,52 +6,21 @@ using DigitalRubyShared;
 
 public class FlickController : MonoBehaviour
 {
-    [SerializeField] Transform _swipeVfx;
-    [SerializeField] Match3Controller _match3Controller;
-    [SerializeField] SwipeController _swipeController;
+    #region FIELDS
+    [SerializeField] Transform _swipeVfx;   
     [SerializeField] float _flickLength = 0.5f;
+    
     public int SwipeTouchCount = 1;
     public SwipeGestureRecognizerEndMode SwipeMode = SwipeGestureRecognizerEndMode.EndImmediately;
     [Range(0.0f, 10.0f)] public float SwipeThresholdSeconds;
-    public GameObject Image;
-    public float flickForce = 20f;
     public bool _updateTurns;
+    #endregion
+
+    float flickForce; // change if need to tune
     SwipeGestureRecognizer swipe;
     GameObject _ball;
     List<GameObject> _balls;
-
-    void Start()
-    {
-        swipe = new SwipeGestureRecognizer();
-        swipe.StateUpdated += Swipe_Updated;
-        swipe.DirectionThreshold = 0f;
-        swipe.MinimumDistanceUnits = _flickLength;
-        swipe.MinimumNumberOfTouchesToTrack = swipe.MaximumNumberOfTouchesToTrack = SwipeTouchCount;
-        swipe.PlatformSpecificView = Image;
-        swipe.ThresholdSeconds = SwipeThresholdSeconds;
-        FingersScript.Instance.AddGesture(swipe);
-        LongPressGestureRecognizer tap = new LongPressGestureRecognizer();
-        tap.MinimumDurationSeconds = 0f;
-        tap.StateUpdated += Tap_Updated;
-        FingersScript.Instance.AddGesture(tap);
-        tap.AllowSimultaneousExecutionWithAllGestures();
-        swipe.AllowSimultaneousExecutionWithAllGestures();
-
-        _balls = new List<GameObject>();
-    }
-
-    void Update()
-    {
-        swipe.MinimumNumberOfTouchesToTrack = swipe.MaximumNumberOfTouchesToTrack = SwipeTouchCount;
-        swipe.EndMode = SwipeMode;
-
-        if (_updateTurns)
-        {
-            LevelManager.Instance._currentLevel.GetComponent<Level>()._turnsRemaining--;
-            LevelManager.Instance._currentLevel.GetComponent<Level>().CheckTurnsLeft();
-            _updateTurns = false;
-        }
-    }
+    Rigidbody2D _rb;
 
     void Tap_Updated(GestureRecognizer gesture)
     {
@@ -64,17 +33,64 @@ public class FlickController : MonoBehaviour
                 _swipeController.enabled = false;
             }
             else if (_ball.CompareTag("BG"))
-            {
                 _swipeController.enabled = true;
-            }
-
             if (_ball.GetComponent<BallController>() == null) return;
             _match3Controller._isFlicking = true;
         }
-
         if (gesture.State == GestureRecognizerState.Ended)
-        {
             _match3Controller._isFlicking = false;
+    }
+
+    void Swipe_Updated(GestureRecognizer gesture)
+    {
+        SwipeGestureRecognizer swipe = gesture as SwipeGestureRecognizer;
+        if (swipe.State == GestureRecognizerState.Ended)
+        {
+            if (_ball == null || _ball.GetComponent<BallController>() == null) return;
+            var rb = _ball.GetComponent<Rigidbody2D>();
+            if (rb != _rb)
+            {
+                rb.AddForce(new Vector2(swipe.DeltaX, swipe.DeltaY) * swipe.Speed * flickForce * Time.deltaTime);
+                rb = null;
+                _ball.GetComponent<FlickController>()._updateTurns = true;
+            }
+            _ball = null;
+            _swipeVfx.gameObject.SetActive(false);
+        }
+    }
+
+    #region UNITY
+    GameObject _fingerManager;
+    Match3Controller _match3Controller;
+    SwipeController _swipeController;
+    void Start()
+    {
+        CreateSwipeGesture();
+        CreateLongPressGesture();
+
+        _fingerManager = GameObject.FindGameObjectWithTag("FingerManager");
+        _match3Controller = _fingerManager.GetComponent<Match3Controller>();
+        _swipeController = _fingerManager.GetComponent<SwipeController>();
+
+        _balls = new List<GameObject>();
+        _rb = GetComponent<Rigidbody2D>();
+
+        #if UNITY_EDITOR
+                flickForce = 0.3f;
+        #else
+                flickForce = 0.03f;
+        #endif
+    }
+
+    void Update()
+    {
+        swipe.MinimumNumberOfTouchesToTrack = swipe.MaximumNumberOfTouchesToTrack = SwipeTouchCount;
+        swipe.EndMode = SwipeMode;
+        if (_updateTurns)
+        {
+            LevelManager.Instance._currentLevel.GetComponent<Level>()._turnsRemaining--;
+            LevelManager.Instance._currentLevel.GetComponent<Level>().CheckTurnsLeft();
+            _updateTurns = false;
         }
     }
 
@@ -87,23 +103,26 @@ public class FlickController : MonoBehaviour
             _match3Controller.CheckMatch(_balls);
         }
     }
-
-    void Swipe_Updated(GestureRecognizer gesture)
+    #endregion
+    #region HELPERS
+    void CreateLongPressGesture()
     {
-        SwipeGestureRecognizer swipe = gesture as SwipeGestureRecognizer;
-        if (swipe.State == GestureRecognizerState.Ended)
-        {
-            if (_ball.GetComponent<BallController>() == null) return;
-            var rb = _ball.GetComponent<Rigidbody2D>();
-            if (rb != GetComponent<Rigidbody2D>())
-            {
-                rb.AddForce(new Vector2(swipe.DeltaX, swipe.DeltaY) * swipe.Speed * flickForce * Time.deltaTime);
-                rb = null;
-                _ball.GetComponent<FlickController>()._updateTurns = true;
-            }
-
-            _ball = null;
-            _swipeVfx.gameObject.SetActive(false);
-        }
+        LongPressGestureRecognizer tap = new LongPressGestureRecognizer();
+        tap.MinimumDurationSeconds = 0f;
+        tap.StateUpdated += Tap_Updated;
+        FingersScript.Instance.AddGesture(tap);
+        tap.AllowSimultaneousExecutionWithAllGestures();
+        swipe.AllowSimultaneousExecutionWithAllGestures();
     }
+    void CreateSwipeGesture()
+    {
+        swipe = new SwipeGestureRecognizer();
+        swipe.StateUpdated += Swipe_Updated;
+        swipe.DirectionThreshold = 0f;
+        swipe.MinimumDistanceUnits = _flickLength;
+        swipe.MinimumNumberOfTouchesToTrack = swipe.MaximumNumberOfTouchesToTrack = SwipeTouchCount;
+        swipe.ThresholdSeconds = SwipeThresholdSeconds;
+        FingersScript.Instance.AddGesture(swipe);
+    }
+    #endregion
 }
---
+++ Assets/Scripts/Controllers/GrabController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using DigitalRubyShared;
+
+public class GrabController : MonoBehaviour
+{
+    #region FIELDS
+    [SerializeField] Transform _swipeVfx;
+    [SerializeField] float _sizeMultiplier = 2f;
+    #endregion
+
+    GrabController _ball;
+    void Tap_Updated(GestureRecognizer gesture)
+    {
+
+        if (gesture.State == GestureRecognizerState.Began)
+        {
+            _ball = FingersUtilityExtensions.GetTouchedObject(gesture).GetComponent<GrabController>();
+            if (_ball.CompareTag("Ball"))
+            {
+                _ball.GetComponent<GrabController>().Expand();
+            }
+        }
+
+        if (gesture.State == GestureRecognizerState.Ended)
+        {
+            if (_ball == null) return;
+            _ball.GetComponent<GrabController>().Shrink();
+            _ball = null;
+        }
+    }
+
+    public void Expand()
+    {
+        _scale = true;
+        _t = 0;
+        _timeDirection = 1f;
+        _startScale = _ball.transform.localScale;
+        _targetScale = (_ball.transform.localScale * _sizeMultiplier);
+    }
+
+    public void Shrink()
+    {
+        _scale = true;
+        _t = 0;
+        _timeDirection = -1f;
+        _startScale = _ball.transform.localScale;
+        _targetScale = new Vector3(1, 1, 1);
+    }
+
+    #region UNITY
+    GameObject _fingerManager;
+    void Start()
+    {
+        _fingerManager = GameObject.FindGameObjectWithTag("FingerManager");
+        _startScale = new Vector3();
+        CreateLongPressGesture();
+    }
+
+    bool _scale = false;
+    Vector3 _startScale, _targetScale;
+    void Update()
+    {
+        if (!_scale || _ball == null) return;
+        ChangeBallScale(_timeDirection);
+    }
+
+    #endregion
+    #region HELPERS
+    float _t = 0; // lerp time
+    float _timeDirection; // lerp anim direction
+    void ChangeBallScale(float timeDirection)
+    {
+        if (_t < 1)
+        {
+            _t += (Time.deltaTime * timeDirection);
+            _ball.transform.localScale = Vector3.Lerp(_startScale, _targetScale, _t);
+        }
+        else
+        {
+            _scale = false;
+        }
+    }
+    void CreateLongPressGesture()
+    {
+        LongPressGestureRecognizer tap = new LongPressGestureRecognizer();
+        tap.MinimumDurationSeconds = 0f;
+        tap.StateUpdated += Tap_Updated;
+        FingersScript.Instance.AddGesture(tap);
+        tap.AllowSimultaneousExecutionWithAllGestures();
+    }
+    #endregion
+}
